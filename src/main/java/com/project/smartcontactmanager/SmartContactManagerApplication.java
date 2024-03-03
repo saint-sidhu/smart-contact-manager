ded4955e5d1a4128ad3391700f1472c60943d6d4
saint-sidhu <rreed8249@gmail.com>
2024-03-03 19:29:34 +0530

    First commit

--- src/main/java/com/project/smartcontactmanager/SmartContactManagerApplication.java
+++ src/main/java/com/project/smartcontactmanager/SmartContactManagerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartContactManagerApplication {
-
+	//checking commit
 	public static void main(String[] args) {
 		SpringApplication.run(SmartContactManagerApplication.class, args);
 	}
